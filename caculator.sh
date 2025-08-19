9c62e4eda37b4467a1567bfe54d1d4d943a7e1c9
manjunatha <manjuc.28.06@gmail.com>
2025-08-19 18:49:46 +0530

    git commit --amend command

--- caculator.sh
+++ caculator.sh
@@ -1,2 +1,4 @@
 #i
-#hello i made some changes so that it should show at least some of the changes 
+#hello i made some changes so that it should show at least some of the changes
+
+#this is second code which we are writing 
