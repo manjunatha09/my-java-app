1adc5ccdd45897449daec8216e20238124963a6c
manjunatha09 <manjuc.28.06@gmail.com>
2026-06-06 11:38:16 +0530

    this is new jenkins file and my first pipeline pratice

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins Pipeline");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
